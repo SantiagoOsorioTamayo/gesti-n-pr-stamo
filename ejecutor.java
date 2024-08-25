6bfbe80fff677f6fec0eab22899afa5a7542ab66
SantiagoOsorioTamayo <santiosotama20@gmail.com>
2024-08-25 17:39:12 -0500

    Delete ejecutor.java

--- ejecutor.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class ejecutor {
-    public static void main(String[] args) throws IOException {
-        menu menu = new menu(); //instancia de la clase menú
-        menu.crear_menu_principal();
-    }
-}
