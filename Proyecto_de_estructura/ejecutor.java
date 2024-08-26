525a3bf8d47aba50eaf344445ae682c3a9fff1af
SantiagoOsorioTamayo <santiosotama20@gmail.com>
2024-08-26 10:51:42 -0500

    Delete Proyecto_de_estructura directory

--- Proyecto_de_estructura/ejecutor.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class ejecutor {
-    //ejecución de todo el proyecto
-    public static void main(String[] args) throws IOException {
-        menu me = new menu(); //instancia de la clase menú
-        me.crear_menu_principal();
-    }
-}
